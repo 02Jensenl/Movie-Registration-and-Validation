036957dda30f8c7f8b72397a26d5cd738f6a9ad8
L. Jensen <02Jensenl@github.com>
2020-11-15 20:28:30 -0500

    initial commit

---
+++ Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegMovie.Models
+{
+    public class Movie
+    {
+        [Range(1, 1000)]
+        public int ID { get; set; }
+
+
+        [Required]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Length of the title should be under 50 characters")]
+        public string Title { get; set; }
+
+
+        [Required (ErrorMessage = "Must pick a genre")]
+        public string Genre { get; set; }
+
+        [Range(1880, 2020, ErrorMessage = "Value must be between 1880 and 2020")]
+        public int Year { get; set; }
+        
+        [Required]
+        public List<string> Actors { get; set; }
+
+        [Required]
+        public List<string> Director { get; set; }
+    }
+}
